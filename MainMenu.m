d5bcd36cf375ceaf96e0a396302eb30b95972e43
ReinhartLab <cogneuroReinhart@gmail.com>
2018-08-08 22:18:47 -0400

    Change

--- MainMenu.m
+++ MainMenu.m
@@ -83,6 +83,7 @@
 %looks at correlation between ERP and Freq data.
 %% MainMenu
 Deci                    = [];
+Deci.File = mfilename;
 Deci.Folder.Raw         = ['C:\Users\Researcher\Dropbox (BOSTON UNIVERSITY)\Non-NAS\raw_data\TimeEst EEG-PS_all_new'];        %Folder with Raw Data files
 Deci.SubjectList        = ['all'];                                      %Cell Array of Subjects Files or 'all' to specify all in Deci.Folder.Raw or 'gui' to choose.
 
@@ -158,14 +159,14 @@ if Deci.Step <= 3
     
     % Trial Artifacts
     Deci.Art.TR.Eye                  = [];               % Comment the rest of .Eye to not do Eye Artifact Trial Rejection
-%     Deci.Art.TR.Eye.Interactive      = 0;                % Enable Interactive Trial Artifact Rejection
-%     Deci.Art.TR.Eye.Chans            = {'BVEOG','RHEOG'};    % 1xChan Cell Array of Ocular Eye Channels
-%     Deci.Art.TR.Eye.Toi              = [-.2 1];          % Time Range of Interest to look for Artifacts
-%     
+    Deci.Art.TR.Eye.Interactive      = 0;                % Enable Interactive Trial Artifact Rejection
+    Deci.Art.TR.Eye.Chans            = {'BVEOG','RHEOG'};    % 1xChan Cell Array of Ocular Eye Channels
+    Deci.Art.TR.Eye.Toi              = [-.2 1];          % Time Range of Interest to look for Artifacts
+    
     Deci.Art.TR.Muscle               = [];               % Comment the rest of .Muscle to not do Muscle Artifact Trial Rejection
-%     Deci.Art.TR.Muscle.Interactive   = 0;                % Enable Interactive Trial Artifact Rejection
-%     Deci.Art.TR.Muscle.Toi           = [-.2 1];          % Time Range of Interest to look for Artifacts
-%     
+    Deci.Art.TR.Muscle.Interactive   = 0;                % Enable Interactive Trial Artifact Rejection
+    Deci.Art.TR.Muscle.Toi           = [-.2 1];          % Time Range of Interest to look for Artifacts
+    
     Deci.Art.Manual = 1;
 
     Artifactor(Deci);
