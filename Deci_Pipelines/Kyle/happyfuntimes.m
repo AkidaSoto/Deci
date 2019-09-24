1aa8413d00425f6bcdfbad4d3c57452b225dc853
ReinhartLab <cogneuro@bu.edu>
2019-09-24 18:55:37 -0400

    asdf
    asdf

--- Deci_Pipelines/Kyle/happyfuntimes.m
+++ Deci_Pipelines/Kyle/happyfuntimes.m
@@ -1,12 +1,3 @@
-function happyfuntimes(dirr)
-
-cdr = CleanDir(dirr);
-for i = 1:length(cdr)
-    files = CleanDir(strcat(dirr+cdr{i}));
-    if exist(strcat(dirr+cdr{i}),'dir')
-        files = files(~ismember(files,CleanDir(strcat(dirr+cdr{i}))));
-    end
-    
-end
+function happyfuntimes(subj)
 
 end
