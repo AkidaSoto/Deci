854f3c16679912fa24cfe39274f72d7b3f8dd376
AkidaSoto <60154963+AkidaSoto@users.noreply.github.com>
2021-11-08 13:18:00 -0500

    push

--- Function/Xtra_Fun/RemoveStimEEGs.m
+++ Function/Xtra_Fun/RemoveStimEEGs.m
@@ -22,16 +22,18 @@ for Each = 1:length(AllFiles)
     cfg.DT = Deci.DT;
     cfg.trialfun = cfg.DT.Type;
     cfg.file_ending = datatype;
-    try
-        Trialcfg = ft_definetrial(cfg);
-    catch
-        continue
-    end
+    Trialcfg = ft_definetrial(cfg);
 
     Trialcfg.detrend = 'yes';
     data = ft_preprocessing(Trialcfg);
     
-    Thresh =    cellfun(@(c) median(range(c,2)),data.trial)  < .2e4;
+    Thresh =    cellfun(@(c) mean(range(c,2)),data.trial)  < .2e4;
+
+    figure;
+    plot(cellfun(@(c) mean(range(c,2)),data.trial));
+    hold on
+    plot(cellfun(@(c) max(var(c,[],2)),data.trial))
+
     TrialThresh = find(Thresh,1,'first');
     TimeThresh = data.sampleinfo(TrialThresh+10,1);
     
@@ -40,16 +42,14 @@ for Each = 1:length(AllFiles)
     event = ft_read_event([Dir filesep AllFiles{Each} '.' datatype]);
     dat   = ft_read_data([Dir filesep AllFiles{Each} '.' datatype]);
     
-    FirstTrial = find([event(ismember({event.value},{num2str(Start)})).sample] > TimeThresh,1,'first');
-    FirstMrk = find(ismember({event.value},{num2str(Start)}),FirstTrial);
-    
-    event(FirstMrk(end)).sample;
-    
-    event = event(FirstMrk(end):end);
+    event = StandardizeEventMarkers(event);
+
+    FirstTrial = find([event.sample] > TimeThresh,1,'first');
+    FirstMrk = FirstTrial + find(ismember({event(FirstTrial:end).value},{num2str(Start)}),1,'first') - 1;
+
+    event = event(FirstMrk:end);
     dat = dat(:,event(1).sample-5000:end);
     event= arrayfun(@(c)  setfield(c,'sample',c.sample - [event(1).sample - 5000]),event);
-    
-
     ft_write_data([Dir '_new' filesep  AllFiles{Each}],dat,'header',hdr,'dataformat','brainvision_eeg','event',event);
     
     %%plot
